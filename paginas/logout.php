05f9cb7c3dab9722b556c961d70cca90401e0315
ThiagoDominguez <credgeyt@gmail.com>
2025-02-20 22:03:25 -0300

    documentacion del archivo logout.php

--- paginas/logout.php
+++ paginas/logout.php
@@ -1,4 +1,6 @@
 <?php
+/* `session_start (); `es una función PHP que inicializa una nueva sesión o reanuda la sesión existente.
+Por lo general, se usa al comienzo de un script para comenzar o reanudar una sesión, lo que le permite que se almacene y recupere las variables de sesión en varias páginas. */
 session_start();
 session_unset(); // Elimina todas las variables de sesión
 session_destroy(); // Destruye la sesión
